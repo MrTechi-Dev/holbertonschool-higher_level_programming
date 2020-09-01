6a57c4087929e5cbec983fdedb2d3adb87a58b7c
Andres802 <reachandres@gmail.com>
2020-09-01 04:01:35 +0000

    the C linked list

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * check_cycle - function to check if it is a singly linked list or it has a
+ * cycle in it
+ * @list: list
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *current = list, *nextp = list;
+
+	while (current && nextp && nextp->next)
+	{
+		current = current->next;
+		nextp = nextp->next->next;
+		if (current == nextp)
+			return (1);
+	}
+	return (0);
+}
