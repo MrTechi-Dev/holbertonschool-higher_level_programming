eeef184e93e3283e332cebf1fd578688d5d11471
Andres802 <reachandres@gmail.com>
2020-09-01 21:55:11 +0000

    modulo number function

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_digit = number % 10
+
+if number < 0:
+    last_digit = abs(number) % 10
+    # abd function corrects the last digit number issue on negative numbers
+    last_digit = -last_digit
+print("Last digit of", number, "is", last_digit, "and is", end=" ")
+if last_digit > 5:
+    print("greater than 5")
+elif last_digit == 0:
+    print("0")
+elif last_digit < 6:
+    print("less than 6 and not 0")
