6d5262de85134c32f80a9b6602d54e75cbc5a4fe
Andres802 <reachandres@gmail.com>
2020-09-02 01:08:06 +0000

    whitespace fix

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 9):
-    for j in range (i + 1, 10):
+    for j in range(i + 1, 10):
         if i == 8 and j == 9:
             print("{:d}{:d}".format(i, j), sep='')
         else:
