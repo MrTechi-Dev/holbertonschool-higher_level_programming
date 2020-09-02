8bad69ed904bf1c84fcc5e24042eaba1308be96b
Andres802 <reachandres@gmail.com>
2020-09-02 04:56:13 +0000

    Fix print double

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-for g in range(0, 26):
-    if (g % 2 == 0):
-        print("{:s}".format(chr(122 - g)), end="")
-    else:
-        print("{:s}".format(chr(122 - g - 32)), end="")
+for i in range(122, 96, -1):
+    print("{:c}".format((i - 32) if i % 2 != 0 else i), end='')
