e77306ea247d6d8cc0e52e446a887b35dda443c3
Andres802 <reachandres@gmail.com>
2020-09-03 22:00:42 +0000

    User permissions fixed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    n = len(sys.argv) - 1
     argv = sys.argv
+    n = len(sys.argv) - 1
+
 if n == 0:
     print("0 arguments")
 else:
