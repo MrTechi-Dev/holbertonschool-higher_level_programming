87b79ded4dc12a585483665f29db8cbce30c4bdf
Andres802 <reachandres@gmail.com>
2020-09-03 22:07:35 +0000

    Sintax Fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,12 +5,12 @@ if __name__ == "__main__":
     n = len(sys.argv) - 1
 
 if n == 0:
-    print("0 arguments")
+    print("0 arguments.")
 else:
     if n == 1:
         print("{} argument:".format(n))
         print("{}: {}".format(n, argv[n]))
     else:
-        print("{} argument:".format(n))
+        print("{} arguments:".format(n))
         for i in range(1, n + 1):
             print("{}: {}".format(i, argv[i]))
