9d142d043276dfda44500d95b509afbc456abaf5
Andres802 <reachandres@gmail.com>
2020-09-08 22:23:47 +0000

    replace elemnt in the list

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        return (my_list)
+    elif idx > len(my_list) - 1:
+        return (my_list)
+    else:
+        my_list.insert(idx, element)
+        my_list.pop(4)
+        return(my_list)
