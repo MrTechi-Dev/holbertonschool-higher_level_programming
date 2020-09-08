7f6db531cb7fe21eb564439ee34f55e738e4b9d5
Andres802 <reachandres@gmail.com>
2020-09-08 22:01:33 +0000

    space operator PEP8

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 def element_at(my_list, idx):
     if idx < 0:
         return
-    elif idx > len(my_list) -1:
+    elif idx > len(my_list) - 1:
         return
     else:
         return (my_list[idx])
