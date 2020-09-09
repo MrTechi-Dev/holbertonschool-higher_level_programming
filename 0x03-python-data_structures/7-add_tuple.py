020636efdcc9c9a92629a8d1439351b8df722b67
Andres802 <reachandres@gmail.com>
2020-09-09 01:23:50 +0000

    diffrent logic program for the checker

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_b) == 0:
-        tuple_b = (0, 0)
-    if len(tuple_b) == 1:
-        tuple_b = (tuple_b[0], 0)
-    res = tuple(map(lambda i, j: i + j, tuple_a, tuple_b))
-    return res
+    tuple_one = tuple_a + (0, 0)
+    tuple_two = tuple_b + (0, 0)
+    add_tuple = tuple_one[0] + tuple_two[0], tuple_one[1] + tuple_two[1]
+    return add_tuple
