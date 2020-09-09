191df8508edf30bf6e338203edac3c14591e3a13
Andres802 <reachandres@gmail.com>
2020-09-09 01:38:49 +0000

    edge case adde

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence:
-        return len(sentence), sentence[0]
+    if len(sentence) == 0:
+        return (None)
+    return len(sentence), sentence[0]
