fa6e495f6421b32a6130f23da4643119fd5fcddd
Andres802 <reachandres@gmail.com>
2020-09-11 04:49:24 +0000

    Convert to integer from roman numnber

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -8,7 +8,7 @@ def roman_to_int(roman_string):
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     for i in range(len(roman_string)):
         if i > 0 and roman[roman_string[i]] > roman[roman_string[i - 1]]:
-            x= x + roman[roman_string[i]] - 2 * roman[roman_string[i - 1]]
+            x = x + roman[roman_string[i]] - 2 * roman[roman_string[i - 1]]
         else:
-            x= x + roman[roman_string[i]]
+            x = x + roman[roman_string[i]]
     return x
