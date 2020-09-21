8371acd56eb6a835ded042dbaa026dfe20e058d6
Andres802 <reachandres@gmail.com>
2020-09-21 22:03:43 +0000

    fixed value of x ro None

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    x = 0
+    x = None
     try:
         x = a / b
     except ZeroDivisionError:
-            pass
+            return None
     finally:
             print("Inside result: {}".format(x))
     return x
