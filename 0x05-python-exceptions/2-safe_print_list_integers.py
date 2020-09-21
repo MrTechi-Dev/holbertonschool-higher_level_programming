64361b229637e222bdcf1ccecd1aaf86ba334fcf
Andres802 <reachandres@gmail.com>
2020-09-21 21:01:21 +0000

    integer selction :d inside the punt func

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
     counter = 0
     for i in range(0, x):
         try:
-            print("{}".format(my_list[i]), end="")
+            print("{:d}".format(my_list[i]), end="")
             count = count + 1
         except (TypeError, ValueError):
             pass
