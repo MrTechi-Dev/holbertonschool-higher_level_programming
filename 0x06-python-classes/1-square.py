3f2b9f9d27262583a62de4a826aa0b5f04003c5f
Andres802 <reachandres@gmail.com>
2020-09-23 02:36:43 +0000

    Comments fixed

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This is a class Square defined by size."""
+
+
+class Square:
+    "Def"
+    def __init__(self, size):
+        """The initialization method of the square class
+        Args:
+            size (int): Is the type int private attribute
+        """
+        self.__size = size
