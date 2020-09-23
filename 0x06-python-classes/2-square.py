651784794d3316773494db45e4af7c85332c181e
Andres802 <reachandres@gmail.com>
2020-09-23 02:38:05 +0000

    Comments fixed

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -2,7 +2,15 @@
 
 
 class Square:
+    """Inizialitation class"""
     def __init__(self, size=0):
+        """The initialization method of the square class
+        Args:
+            size (int): Is the type int private attribute
+        Raises:
+            TypeError: If size is not an integer
+            ValueError: If size is less than 0
+        """
         self.__size = size
         if type(size) is not int:
             raise TypeError("size must be an integer")
