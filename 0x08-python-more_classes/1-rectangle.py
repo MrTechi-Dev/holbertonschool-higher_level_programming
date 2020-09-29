8c7fd68de9ad5c0c8bf3678452dde955718f160c
Andres802 <reachandres@gmail.com>
2020-09-29 03:39:54 +0000

    if statment change

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -28,17 +28,17 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """setter"""
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("width must be an integer")
-        if value < 0:
+        elif value < 0:
             raise valueError("width must be >= 0")
         self.__width = value
 
     @height.setter
     def height(self, value):
         """setter"""
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("height must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
