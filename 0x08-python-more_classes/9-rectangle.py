be6f9734449ff4d10b3bd4753f0002dde3445d61
Andres802 <reachandres@gmail.com>
2020-09-29 16:19:48 +0000

    Value error fixed

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -34,7 +34,7 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
-            raise valueError("width must be >= 0")
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @height.setter
