6f055202c4102878afbe9f4713e3cd7538995a16
Andres802 <reachandres@gmail.com>
2020-10-07 00:10:43 +0000

    class added instead of import module

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,7 +2,18 @@
 """ lookup method """
 
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+class BaseGeometry():
+    """class"""
+    def area(self):
+        """ Raises Exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates value"""
+        if type(value) is not int:
+            raise TypeError(name + " must be an integer")
+        if value <= 0:
+            raise ValueError(name + " must be greater than 0")
 
 
 class Rectangle(BaseGeometry):
