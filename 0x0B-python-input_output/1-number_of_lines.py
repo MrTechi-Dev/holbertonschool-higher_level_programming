a55f6984c23f92a29d6fbe1cabbda2740479398b
Andres802 <reachandres@gmail.com>
2020-10-08 02:25:56 +0000

    File input and output Json file

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -5,7 +5,7 @@ Basic file number lines
 
 
 def number_of_lines(filename=""):
-    """returnd number of lones of a text file"""
-    with open("my_file_0.txt", encoding="utf-8") as myFile:
+    """return number of lones of a text file"""
+    with open(filename, encoding="utf-8") as myFile:
         readmyFile = len(myFile.readlines())
         return readmyFile
