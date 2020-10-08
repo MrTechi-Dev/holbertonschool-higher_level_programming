55769cf25ebec0986d1a6be117e25d3ce0d27c47
Andres802 <reachandres@gmail.com>
2020-10-08 02:36:21 +0000

    Fix import json

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
-"""
-Basic file jason
-"""
-import sys
-import json
+""" script that adds arguments to a python list and saven them """
+import os.path
+from sys import argv
+""" sys allows me to receive the string to add in the file from the cl
+for accessing the filesystem os.path """
 
 
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-try:
-    list9 = load_from_json_file("add_item.json")
-except:
-    list9 = []
-for i in range(1, len(sys.argv)):
-    list9.append(sys.argv[i])
-
-save_to_json_file(list9, "add_item.json")
+p_list = []
+if os.path.exists("add_item.json"):
+    p_list = load_from_json_file("add_item.json")
+save_to_json_file(p_list + argv[1:], "add_item.json")
