9d1aab158b9891a53896137792dcad9836ad7c58
Andres802 <reachandres@gmail.com>
2020-10-08 02:29:58 +0000

    Fix w for a

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -6,5 +6,5 @@ Basic file reading and append
 
 def append_write(filename="", text=""):
     """ write a new file and count number of chars"""
-    with open(filename, 'w', encoding="utf-8") as file:
+    with open(filename, 'a', encoding="utf-8") as file:
         return file.write(text)
