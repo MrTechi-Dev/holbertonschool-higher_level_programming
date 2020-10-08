5d42f9c0b01d1a039d5152f812485d9f72f15fd1
Andres802 <reachandres@gmail.com>
2020-10-08 02:22:17 +0000

    File input and output Json file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,9 +5,6 @@ Basic file reading
 
 
 def read_file(filename=""):
-    """
-    Function to read a file and print it out
-    """
-    f = open(filename, 'r')
-    for line in f:
-        print('{}'.format(line), end='')
+    """read the file"""
+    with open("my_file_0.txt", encoding="utf-8") as myFile:
+        print(myFile.read())
