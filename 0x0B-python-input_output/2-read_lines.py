56a30c2663926233fba4ee0e581657d6efd4d859
Andres802 <reachandres@gmail.com>
2020-10-08 02:28:13 +0000

    File input and output Json file

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -6,13 +6,10 @@ Basic file reading
 
 def read_lines(filename="", nb_lines=0):
     """reads n number of lines from the .txt file"""
-    with open("my_file_0.txt", 'r', encoding="utf-8") as myFile:
-        if nb_lines <= 0:
-            file = myFile.readlines()
-            for i in file:
-                print(i, end='')
-
+    nlines = len(open(filename).readlines())
+    with open(filename, encoding="utf-8") as f:
+        if nb_lines > 0 and nb_lines < nlines:
+            for lines in range(nb_lines):
+                print(f.readline(), end="")
         else:
-            file = myFile.readlines()[0:nb_lines]
-            for i in file:
-                print(i, end='')
+            print(f.read(), end="")
