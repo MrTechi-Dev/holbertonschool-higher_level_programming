3477a7c16d8004cf19b358621a2e2594f53ed43a
Andres802 <reachandres@gmail.com>
2020-11-19 05:00:10 +0000

    fix context garbage

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -5,13 +5,3 @@ FROM tv_shows AS tv
 LEFT JOIN tv_show_genres AS ge
 ON ge.show_id = tv.id
 WHERE ge.genre_id IS NULL ORDER BY tv.title, ge.genre_id ASC;
-
-    © 2020 GitHub, Inc.
-    Terms
-    Privacy
-    Security
-    Status
-    Help
-    Contact GitHub
-    Pricing
-    API
