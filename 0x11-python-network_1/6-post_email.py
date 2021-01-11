35e4d57e853c8d1edbfb042827eefea69f64697c
Andres802 <reachandres@gmail.com>
2021-01-11 23:34:59 +0000

    Get a post resquest email

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL and an email address,
+sends a POST request to the passed URL with the email as a
+parameter, and finally displays the body of the response.t"""
+
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+    result = requests.post(url, {'email': email})
+    print(result.text)
