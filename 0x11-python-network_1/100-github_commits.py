1f6b1de0f33d7f1da1003ffd6d95dd84f7c67134
Andres802 <reachandres@gmail.com>
2021-01-13 17:57:45 +0000

    holberto challange API github

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Python script that takes 2 arguments in order to solve this challenge.
+    The first argument will be the repository name
+    The second argument will be the owner name
+    You must use the packages requests and sys
+    You are not allowed to import packages other than requests and sys
+    You don’t need to check arguments passed to the script (number or type)
+"""
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+
+    repo = argv[1]
+    owner = argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
+    c = requests.get(url)
+    j = c.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(j[i].get('sha'),
+                  j[i].get('commit').get('author').get('name')))
+    except:    
+        pass
