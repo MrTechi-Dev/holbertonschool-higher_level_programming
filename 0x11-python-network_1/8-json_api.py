7ff65c359dee8c66df056a3ee9a103a3ad18d308
Andres802 <reachandres@gmail.com>
2021-01-13 17:00:02 +0000

    script fix

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-"""Python script that takes in a letter and sends a 
-POST request to http://0.0.0.0:5000/search_user with 
+"""Python script that takes in a letter and sends a
+POST request to http://0.0.0.0:5000/search_user with
 the letter as a parameter."""
 
 if __name__ == '__main__':
     import requests
     from sys import argv
 
-    
     if len(argv) > 1:
         dic = {'q': argv[1]}
     else:
