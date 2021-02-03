f74eddd09e6afdc229fc984a2b22ca023ec8ae0a
Andres802 <reachandres@gmail.com>
2021-02-03 04:29:12 +0000

    X  square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+if (process.argv[2] && Number(process.argv[2])) {
+  for (let i = 0; i < process.argv[2]; i++) {
+    console.log('X'.repeat(process.argv[2]));
+  }
+} else {
+  console.log('Missing size');
+}
