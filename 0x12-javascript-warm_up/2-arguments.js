819672b794f5ea02366c1917f89765bd144d292e
Andres802 <reachandres@gmail.com>
2021-02-03 03:33:06 +0000

    semistandar fix all

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
-if (!process.argv[2]) 
-  {
-    console.log('No argument');
-  } else if (process.argv[2] && !process.argv[3]) 
-  {
-    console.log('Argument found');
-  } else 
-  {
-    console.log('Arguments found');
-  }
+if (!process.argv[2]) {
+  console.log('No argument');
+} else if (process.argv[2] && !process.argv[3]) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
